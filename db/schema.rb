ba66bd9b611d5a3e51670fd7b811ae5240de5969
Truong Duc Quang <truongducquangk6@gmail.com>
2025-07-17 23:01:55 +0700

    chapter_3_4_5
    chapter_3_4_5
    
    chapter_3_4_5
    
    chapter_3_4_5"

--- db/schema.rb
+++ db/schema.rb
@@ -17,4 +17,11 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_17_165845) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
