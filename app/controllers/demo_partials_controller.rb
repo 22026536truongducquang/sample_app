ac263ac6f71b1f7a5b992fcfaa742f114b3de455
Truong Duc Quang <truongducquangk6@gmail.com>
2025-07-22 16:38:47 +0700

    chapter_7

--- app/controllers/demo_partials_controller.rb
+++ app/controllers/demo_partials_controller.rb
@@ -1,19 +1,11 @@
 class DemoPartialsController < ApplicationController
   def new
-<<<<<<< HEAD
     @zone = t("demo_partials.form.zone_label")
-=======
-    @zone = "Zone new action"
->>>>>>> 3d7a312 (Update pull_request_template.md)
     @date = Time.zone.today
   end
 
   def edit
-<<<<<<< HEAD
     @zone = t("demo_partials.form.zone_label")
-=======
-    @zone = "Zone edit action"
->>>>>>> 3d7a312 (Update pull_request_template.md)
     @date = Time.zone.today - 4
   end
 end
