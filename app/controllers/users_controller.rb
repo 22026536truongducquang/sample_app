7ed73129be5b15d31a652ead22be358532c46905
Truong Duc Quang <truongducquangk6@gmail.com>
2025-07-23 16:00:44 +0700

    chapter_7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      flash[:success] = t("welcome_to_the_sample_app!")
+      flash[:success] = t("users.created.success")
       redirect_to @user, status: :see_other
     else
       render :new, status: :unprocessable_entity
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-<<<<<<< HEAD
 import "@hotwired/turbo-rails"
 import "controllers"
-=======
-import '@hotwired/turbo-rails'
-import 'controllers'
->>>>>>> 3d7a312 (Update pull_request_template.md)
-
