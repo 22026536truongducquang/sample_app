5e0521768d0dbcacb463c5a1038d0204d5f847f6
Truong Duc Quang <truongducquangk6@gmail.com>
2025-07-25 09:51:53 +0700

    chapter_8

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params.dig(:session, :email)&.downcase)
+
+    if user&.authenticate(params.dig(:session, :password))
+      # Log the user in and redirect to the user's show page.
+      reset_session
+      log_in user
+      flash[:success] = t(".login_success")
+      redirect_to user, status: :see_other
+    else
+      # Create an error message and re-render the login page.
+      flash.now[:danger] = t(".invalid_email_or_password")
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    log_out
+    flash[:success] = t(".logout_success")
+    redirect_to root_url, status: :see_other
+  end
+end
