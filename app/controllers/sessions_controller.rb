14144e0cf737d70e4b91b9b87cb59b59e87e099b
Truong Duc Quang <truongducquangk6@gmail.com>
2025-07-26 17:29:17 +0700

    chapter_9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,12 +9,12 @@ class SessionsController < ApplicationController
     log_in @user
     if params.dig(:session,
                   :remember_me) == REMEMBER_ME_SELECTED
-      remember(user)
+      remember @user
     else
-      forget(user)
+      forget @user
     end
     flash[:success] = t(".login_success")
-    redirect_to user, status: :see_other
+    redirect_to user_path(@user), status: :see_other
   end
 
   def destroy
